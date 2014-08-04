d3365023aa0a2ebbb3994c55891fa41a08cb26d9
Shane Osbourne <shane.osbourne8@gmail.com>
2014-08-04 22:57:01 +0100

    Added SASS files & Bower.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,63 @@
-var gulp       = require("gulp");
-var karma      = require('gulp-karma');
-var jshint     = require('gulp-jshint');
-var contribs   = require('gulp-contribs');
-var browserify = require('gulp-browserify');
-var rename     = require('gulp-rename');
+var gulp        = require("gulp");
+var karma       = require('gulp-karma');
+var jshint      = require('gulp-jshint');
+var contribs    = require('gulp-contribs');
+var sass        = require("gulp-ruby-sass");
+var autoprefix  = require("gulp-autoprefixer");
+var browserify  = require('gulp-browserify');
+var rename      = require('gulp-rename');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
 
-var testFiles = [
-    'test/todo.js'
-];
-
+/**
+ * Lint all JS files
+ */
 gulp.task('lint', function () {
-    gulp.src(['test/client/specs/**/*.js', 'lib/js/scripts/*.js', 'index.js'])
+    return gulp.src(['test/client/specs/**/*.js', 'lib/js/scripts/*.js', 'index.js'])
         .pipe(jshint('test/.jshintrc'))
         .pipe(jshint.reporter("default"))
         .pipe(jshint.reporter("fail"))
 });
 
+/**
+ * Update Contributors list
+ */
 gulp.task('contribs', function () {
     gulp.src('README.md')
         .pipe(contribs())
         .pipe(gulp.dest("./"))
 });
 
+
+/**
+ * Build the app.
+ */
 gulp.task('browserify', function () {
-    gulp.src('lib/js/scripts/index.js')
+    return gulp.src('lib/js/scripts/index.js')
         .pipe(browserify())
         .pipe(rename("app.js"))
         .pipe(gulp.dest("./lib/js/dist"))
+});
+
+/**
+ * Start BrowserSync
+ */
+gulp.task('browser-sync', function () {
+    browserSync({
+        proxy: "http://localhost:3001/",
+        files: "lib/*.html"
+    })
+});
+
+/**
+ * Compile CSS
+ */
+gulp.task('sass', function () {
+    return gulp.src('lib/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(autoprefix())
+        .pipe(gulp.dest('lib/css'))
+        .pipe(reload({stream:true}));
 });
 
 gulp.task('default', ["lint"]);
@@ -35,4 +66,9 @@ gulp.task('build', ["browserify", "lint"]);
 
 gulp.task('dev', ["browserify"], function () {
     gulp.watch("lib/js/scripts/**/*.js", ["browserify"]);
+});
+
+gulp.task('dev-frontend', ["browser-sync"], function () {
+    gulp.watch("lib/js/scripts/**/*.js", ["browserify", reload]);
+    gulp.watch("lib/scss/**/*.scss", ["sass"]);
 });
