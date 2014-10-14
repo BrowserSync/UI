7ad09bafe43608e75ac1098e72b26d70cf950498
Shane Osbourne <shane@wearejh.com>
2014-10-14 09:56:29 +0100

    add fixtures and plugin example

--- example.server.js
+++ example.server.js
@@ -1,12 +1,17 @@
-var path   = "/Users/shakyshane/sites/os-browser-sync";
-var cp     = require("./index");
-var bs     = require("browser-sync");
+var path         = "/Users/shakyshane/sites/os-browser-sync";
+var cp           = require("./index");
+var bs           = require("browser-sync");
+var htmlInjector = require("bs-html-injector");
+
 //var client = require("/Users/shakyshane/sites/browser-sync-modules/browser-sync-client");
 //
 //client["plugin:name"] = "client:script";
 
+bs.use(htmlInjector, {
+    logLevel: "debug",
+    files: ["test/fixtures/*.html"]
+});
 bs.use(cp);
-//bs.use(client);
 
 bs({
     server: {
