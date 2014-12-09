f924d213b4e55b2c42c6ae18d9c86ddc09ead697
Shane Osbourne <shane.osbourne8@gmail.com>
2014-12-09 23:41:23 +0000

    Add markup for plugins page

--- gulpfile.js
+++ gulpfile.js
@@ -90,11 +90,12 @@ gulp.task('bs-inject', function () {
  */
 gulp.task('build-src', function (done) {
     //var file = fs.readFileSync("lib/src/server-info.hbs", "utf-8");
-    var file = fs.readFileSync("lib/src/history.hbs", "utf-8");
+    //var file = fs.readFileSync("lib/src/history.hbs", "utf-8");
+    var file = fs.readFileSync("lib/src/plugins.hbs", "utf-8");
     crossbow.clearCache();
     //crossbow.addPage("server-info.hbs", file);
-    crossbow.addPage("history.hbs", file);
-    crossbow.compileOne("history.hbs", {cwd: "lib/src"}, function (err, out) {
+    crossbow.addPage("plugins.hbs", file);
+    crossbow.compileOne("plugins.hbs", {cwd: "lib/src"}, function (err, out) {
         if (!err) {
             fs.writeFileSync("lib/"  + out.paths.filePath, out.compiled);
         }
