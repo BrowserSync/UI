2af52a7f43b22b57e93471b93b1574739b0f5bc7
Shane Osbourne <shane.osbourne8@gmail.com>
2015-01-19 08:06:10 +0000

    Full end to end test for history item removal

--- test/client/e2e/e2e.history.js
+++ test/client/e2e/e2e.history.js
@@ -3,22 +3,16 @@
  * E2E tests for the History plugin
  *
  */
+describe("History section", function() {
 
-describe('History section', function() {
-
-    var expected, selector, menu, bsUrl;
     var ptor = protractor.getInstance();
-    var url;
 
     beforeEach(function () {
         browser.ignoreSynchronization = true;
     });
-    afterEach(function () {
-    });
+
     it("should list visited urls", function () {
 
-        var instance;
-        var cp;
         var flow = protractor.promise.controlFlow();
         var deferred = protractor.promise.defer();
 
@@ -28,40 +22,83 @@ describe('History section', function() {
 
         flow.execute(function () {
 
-            bs.init({server: "./test/fixtures", open: false, online: false}, function (err, _bs) {
-                var cp = _bs.pluginManager.getReturnValues("UI")[0].value;
-                cp.getServer(function (err, server) {
-                    deferred.fulfill(server);
-                });
+            bs.init({
+                server: "./test/fixtures",
+                open: false,
+                online: false
+            }, function (err, _bs) {
+                _bs.pluginManager.getReturnValues("UI")[0].value
+                    .getServer(function (err, server) {
+                        deferred.fulfill(server.address().port);
+                    });
             });
+
             return deferred.promise;
 
-        }).then(function (server) {
+        }).then(function (port) {
+
             var options = bs.instance.options;
-            var url = "http://localhost:" + server.address().port;
+            var bsUrl   = options.getIn(["urls", "local"]);
+            var url     = "http://localhost:" + port;
+            var elems   = element.all(by.repeater("url in visited track by $index"));
+
             browser.get(url + "/history");
-            var elems = element.all(by.repeater('url in visited track by $index'));
+
             expect(elems.count()).toEqual(0);
 
-            var bsUrl = options.getIn(["urls", "local"]);
             openWindow(ptor, bsUrl);
 
             browser.getAllWindowHandles().then(function (handles) {
-                browser.switchTo().window(handles[1]); // client
+
+                var ui     = handles[0];
+                var client = handles[1];
+
+                browser.switchTo().window(client);
                 browser.get(bsUrl + "/scrolling.html");
-                browser.switchTo().window(handles[0]); // ui
-                elems = element.all(by.repeater('url in visited track by $index'));
+                browser.switchTo().window(ui);
                 expect(elems.count()).toEqual(2);
-                browser.switchTo().window(handles[1]); // client
+                browser.switchTo().window(client);
                 browser.get(bsUrl + "/forms.html");
-                browser.switchTo().window(handles[0]); // ui
-                elems = element.all(by.repeater('url in visited track by $index'));
+                browser.switchTo().window(ui);
                 expect(elems.count()).toEqual(3);
+
+                //ptor.pause();
+
+                var selector      = by.css("#bs-history-list li > [bs-remove]");
+                var deleteButtons = element.all(selector);
+
+                expect(element.all(by.css("#bs-history-empty")).count()).toBe(0);
+
+                deleteButtons.get(0).click();
+
+                expect(elems.count()).toEqual(2);
+
+                browser.sleep(1000);
+
+                deleteButtons.get(0).click();
+
+                expect(elems.count()).toEqual(1);
+
+                browser.sleep(1000);
+
+                deleteButtons.get(0).click();
+
+                browser.sleep(500);
+
+                expect(elems.count()).toEqual(0);
+
+                expect(element.all(by.css("#bs-history-list li")).count()).toBe(0);
+
+                expect(element.all(by.css("#bs-history-empty")).count()).toBe(1);
             });
         });
     });
 });
 
+/**
+ * @param ptor
+ * @param url
+ */
 function openWindow (ptor, url) {
     ptor.executeScript("window.open('%s')".replace("%s", url));
 }
