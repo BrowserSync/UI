81d796d61ffb066e75dce78abd5e5701b4f88c07
Shane Osbourne <shane@wearejh.com>
2015-01-19 09:34:27 +0000

    Upgrade to latest protractor

--- test/client/e2e/e2e.history.js
+++ test/client/e2e/e2e.history.js
@@ -3,11 +3,11 @@
  * E2E tests for the History plugin
  *
  */
-var init = require("./bs-init");
+var init  = require("./bs-init");
+var utils = require("./test-utils");
 
 describe("History section", function() {
 
-    var ptor = protractor.getInstance();
     var bs;
     var port;
 
@@ -23,7 +23,7 @@ describe("History section", function() {
         });
     });
 
-    it("should list visited urls", function () {
+    it("should list visited urls & delete them", function () {
 
         var options = bs.instance.options;
         var bsUrl   = options.getIn(["urls", "local"]);
@@ -34,59 +34,41 @@ describe("History section", function() {
 
         expect(elems.count()).toEqual(0);
 
-        openWindow(ptor, bsUrl);
+        utils.openWindow(browser, bsUrl);
 
         browser.getAllWindowHandles().then(function (handles) {
 
-            var ui     = handles[0];
-            var client = handles[1];
+            var ui             = handles[0];
+            var client         = handles[1];
+            var urls           = ["/scrolling.html", "/forms.html"];
+            var emptyContainer = "#bs-history-empty";
+            var listContainer  = "#bs-history-list";
+            var selector       = by.css(listContainer + " li > [bs-remove]");
+            var deleteButtons  = element.all(selector);
 
             browser.switchTo().window(client);
-            browser.get(bsUrl + "/scrolling.html");
+            browser.get(bsUrl + urls[0]);
             browser.switchTo().window(ui);
             expect(elems.count()).toEqual(2);
             browser.switchTo().window(client);
-            browser.get(bsUrl + "/forms.html");
+            browser.get(bsUrl + urls[1]);
             browser.switchTo().window(ui);
             expect(elems.count()).toEqual(3);
-
-            var selector      = by.css("#bs-history-list li > [bs-remove]");
-            var deleteButtons = element.all(selector);
-
-            expect(element.all(by.css("#bs-history-empty")).count()).toBe(0);
-
+            expect(element.all(by.css(emptyContainer)).count()).toBe(0);
             deleteButtons.get(0).click();
-
             expect(elems.count()).toEqual(2);
-
             browser.sleep(1000);
-
             deleteButtons.get(0).click();
-
             expect(elems.count()).toEqual(1);
-
             browser.sleep(1000);
-
             deleteButtons.get(0).click();
-
             browser.sleep(500);
-
             expect(elems.count()).toEqual(0);
-
-            expect(element.all(by.css("#bs-history-list li")).count()).toBe(0);
-
-            expect(element.all(by.css("#bs-history-empty")).count()).toBe(1);
+            expect(element.all(by.css(listContainer + " li")).count()).toBe(0);
+            expect(element.all(by.css(emptyContainer)).count()).toBe(1);
 
         }).then(function () {
             bs.instance.cleanup();
         });
     });
 });
-
-/**
- * @param ptor
- * @param url
- */
-function openWindow (ptor, url) {
-    ptor.executeScript("window.open('%s')".replace("%s", url));
-}
