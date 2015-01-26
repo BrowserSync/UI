d408aa58b17468027727713b9cf8768ca748db72
Shane Osbourne <shane.osbourne8@gmail.com>
2015-01-26 07:47:13 +0000

    refactor remote-debug spec

--- test/client/e2e/tests/remote-debug.js
+++ test/client/e2e/tests/remote-debug.js
@@ -7,8 +7,6 @@ var utils  = require("./../test-utils");
 
 describe("Remote debug page", function() {
 
-    var selector, menu, headerSelector;
-
     var bs;
     var cp;
     var bsUrl;
@@ -18,8 +16,6 @@ describe("Remote debug page", function() {
     beforeEach(function () {
 
         browser.ignoreSynchronization = true;
-        selector       = '(key, item) in app.ui.menu | orderObjectBy: \'order\'';
-        headerSelector = "h1[bs-heading]";
 
         init(protractor, {
             server: "./test/fixtures",
@@ -27,24 +23,25 @@ describe("Remote debug page", function() {
             online: false
         }).then(function (out) {
             port  = out.port;
-            bs    = out.bs;
+            bs    = out.bs.instance;
             cp    = out.cp;
-            bsUrl = bs.instance.options.getIn(["urls", "local"]);
+            bsUrl = bs.options.getIn(["urls", "local"]);
             cpUrl = "http://localhost:" + out.cp.server.address().port;
         });
     });
 
     afterEach(function () {
-        bs.instance.cleanup();
+        bs.cleanup();
     });
 
     it("should allow elements to be added/removed from clients via the UI", function() {
 
-        var id = bs.instance.options.getIn(["clientFiles", "pesticide", "id"]);
+        var id = bs.options.getIn(["clientFiles", "pesticide", "id"]);
         browser.get(cpUrl + "/remote-debug");
         browser.sleep(1000);
+        var button = element.all(by.css("label[for='cmn-form-pesticide']"));
 
-        element.all(by.css("label[for='cmn-form-pesticide']")).get(0).click();
+        button.get(0).click();
 
         utils.openWindow(browser, bsUrl);
 
@@ -55,7 +52,7 @@ describe("Remote debug page", function() {
             expect(element(by.id(id)).isPresent()).toBeTruthy();
             browser.sleep(500);
             browser.switchTo().window(ui);
-            element.all(by.css("label[for='cmn-form-pesticide']")).get(0).click();
+            button.get(0).click();
             browser.switchTo().window(client);
             browser.sleep(500);
             expect(element(by.id(id)).isPresent()).toBeFalsy();
