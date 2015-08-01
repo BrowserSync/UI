2ef505ac779f2ad5d2296975a1651014e092d13d
Shane Osbourne <shane.osbourne8@gmail.com>
2015-08-01 21:59:34 +0100

    Add regex + function validation

--- rewrite/ui/client.js
+++ rewrite/ui/client.js
@@ -212,21 +212,21 @@
 
     angular
         .module("BrowserSync")
-        .directive('replaceInput', function() {
+        .directive('validFn', function() {
             return {
                 require: 'ngModel',
                 scope: {
-                    replaceInput: "="
+                    validFn: "="
                 },
                 link: function(scope, elm, attrs, ctrl) {
-                    ctrl.$validators.inputfn = function(modelValue, viewValue) {
+                    ctrl.$validators.validFn = function(modelValue, viewValue) {
                         if (ctrl.$isEmpty(modelValue)) {
                             // consider empty models to be valid
                             return true;
                         }
 
                         // String type is always valid
-                        if (scope.replaceInput === 'string') {
+                        if (scope.validFn === 'string') {
                             return true;
                         }
 
@@ -243,5 +243,41 @@
                 }
             };
     });
+
+    angular
+        .module("BrowserSync")
+        .directive('validRegex', function() {
+            return {
+                require: 'ngModel',
+                scope: {
+                    validRegex: "=",
+                    flags: "="
+                },
+                link: function(scope, elm, attrs, ctrl) {
+                    ctrl.$validators.validRegex = function(modelValue, viewValue) {
+                        if (ctrl.$isEmpty(modelValue)) {
+                            // consider empty models to be valid
+                            return true;
+                        }
+
+                        // String type is always valid
+                        if (scope.validRegex === 'string') {
+                            return true;
+                        }
+
+                        try {
+                            var fn = new RegExp(viewValue, scope.flags);
+                            return true;
+                        } catch (e) {
+                            console.log(e);
+                            return false;
+                        }
+
+                        // it is invalid
+                        return false;
+                    };
+                }
+            };
+        });
 
 })(angular);
