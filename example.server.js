3c9733885a67726e1aa56a889bce4472066a5dd5
Shane Osbourne <shane.osbourne8@gmail.com>
2015-11-12 16:13:27 +0000

    Fixing SCSS compiling

--- example.server.js
+++ example.server.js
@@ -1,14 +1,9 @@
 var fs = require('fs');
-var bs = require('/Users/shakyshane/code/browser-sync-core-rewrite')
+//var bs = require('/Users/shakyshane/code/browser-sync-core-rewrite')
+var bs = require('/Users/shakyshane/sites/oss/browser-sync-core')
     .create({
         version: "3.0.0",
-        serveStatic: ['/Users/shakyshane/code/browser-sync-core-rewrite/test/fixtures'],
-        //reloadOnRestart: true,
-        //scheme: "https",
-        //proxy: {
-        //    target: 'https://hype-for-type.static:8890',
-        //    options: {}
-        //},
+        serveStatic: ['/Users/shakyshane/sites/oss/browser-sync-core/test/fixtures'],
         plugins: [
             './index'
         ],
@@ -16,8 +11,18 @@ var bs = require('/Users/shakyshane/code/browser-sync-core-rewrite')
             "test/fixtures/*.html"
         ],
         externals: {
-            clientJs: '/Users/shakyshane/code/bs-client'
-        },
+            //clientJs: '/Users/shakyshane/code/bs-client',
+            clientJs: '/Users/shakyshane/sites/oss/browser-sync-client/' // home imac
+        }
+
+
+        //serveStatic: ['/Users/shakyshane/code/browser-sync-core-rewrite/test/fixtures'],
+        //reloadOnRestart: true,
+        //scheme: "https",
+        //proxy: {
+        //    target: 'https://hype-for-type.static:8890',
+        //    options: {}
+        //},
     }, function (err, out) {
         if (err) {
             return console.log(err);
